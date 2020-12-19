938ed71c1f1f838602b484848df3e2074346e1b2
Aner Lucero <4rgento@gmail.com>
2020-12-19 10:10:05 -0300

    Day 19, generalize the NDPA

--- src-lib/Day19.hs
+++ src-lib/Day19.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Day19 ( runSolution) where
 
 import qualified Data.ByteString as BS
@@ -8,27 +9,37 @@ import Data.Attoparsec.ByteString (Parser)
 import qualified Data.Attoparsec.ByteString as DAB
 import Data.Attoparsec.ByteString.Char8 (anyChar, space, notChar, decimal, string, char, endOfLine)
 import qualified Data.Attoparsec.ByteString.Char8 as DABC8
-import Data.Vector (Vector, (!))
 import qualified Data.Vector as V
 import qualified Data.List as DL
 import Debug.Trace
 import Control.Arrow
 import Control.Monad
+import Data.Map.Strict (Map, (!?))
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromJust)
 
-data Rule
-	= Concat Rule Rule
-	| Union Rule Rule
-	| Terminal Char
-	| NonTerminal Int
+-- Represents rules in a context free grammar
+-- t: is the type for the terminal symbols
+-- n: is the type for the non terminal symbols
+data Rule t n
+	= Concat (Rule t n) (Rule t n)
+	| Union (Rule t n) (Rule t n)
+	| Terminal t
+	| NonTerminal n
 	deriving (Show, Eq)
+
+-- A set of rules for a context free gramar.
+-- All rules have the form Nt -> x
+-- So I index them by the non terminal
+type RuleSet t n = Map n (Rule t n)
 
 -- The states of the NDPA (Non deterministic push down automata)
 data NDPAState = P0 | P1 deriving (Show, Eq)
 
 -- The alphabet to be used in the NDPD's stack
-data Output
-	= StackedT Char -- A stacked terminal
-	| StackedNT Int -- An stacked non terminal
+data Output t n
+	= StackedT t -- A stacked terminal
+	| StackedNT n -- An stacked non terminal
 	deriving (Show, Eq)
 
 -- Given a rule compute what should be added to the stack,
@@ -43,48 +54,53 @@ data Output
 -- m: "a"
 --
 -- There is one alternative so it returns [[StackedT 'a']]
-ruleAlternatives :: Rule -> [[Output]]
+ruleAlternatives :: Rule t n -> [[Output t n]]
 ruleAlternatives (NonTerminal n) = [[StackedNT n]]
 ruleAlternatives (Terminal c) = [[StackedT c]]
 ruleAlternatives (Union r1 r2) = concatMap ruleAlternatives [r1, r2]
 ruleAlternatives (Concat r1 r2) = [concatMap (join . ruleAlternatives) [r1, r2]]
 
-
 -- Compute all configurations that can be deribed from the provided one.
 -- This implements the "free" NDPA for a Context Free Grammar
-yield :: Vector Rule -> NDPAState -> String -> [Output] -> [(NDPAState, (String, [Output]))]
+yield :: (Ord n, Eq t) =>
+	RuleSet t n -> n ->
+	NDPAState -> [t] -> [Output t n] -> [(NDPAState, ([t], [Output t n]))]
 
 -- Transition to P1 pushing the intial symbol of the grammar. This is the only
 -- valid transition for P0
-yield rules P0 ss [] = [(P1, (ss, [StackedNT 0]))]
+yield rules initialSymbol P0 ss [] = [(P1, (ss, [StackedNT initialSymbol]))]
 
 -- Push x for every rule: StackedNT a -> x
-yield rules P1 ss (StackedNT a:os) = (\x -> (P1, (ss, x ++ os))) <$> ruleAlternatives (rules ! a)
+--
+-- Is use the non-total `fromJust` becasue all non termial symbols should be on
+-- the rule set.
+yield rules _ P1 ss (StackedNT a:os) = (\x -> (P1, (ss, x ++ os))) <$> ruleAlternatives (fromJust $ rules !? a)
 
 -- Out of input and with a non-empty stack means there are no more options
-yield rules P1 [] (StackedT _:os) = []
+yield rules _ P1 [] (StackedT _:os) = []
 
 -- Continue if the terminal in the top matches the one at the begining of the
 -- stack (if it is a terminal)
-yield rules P1 (s:ss) (StackedT g:os)
+yield rules _ P1 (s:ss) (StackedT g:os)
 	| s == g = [(P1, (ss, os))]
 	| otherwise = []
 
 -- out of options. Notice that the string may not yet be empty.
-yield _ P1 _ [] = []
-yield _ _ _ _ = []
+yield _ _ P1 _ [] = []
+yield _ _ _ _ _ = []
 
 -- Checks if the string is accepted by the context free grammar defined by the
 -- rules.
 --
 -- It uses a NDPA to compute.
-runNDPA :: Int -> Vector Rule -> String -> Bool
-runNDPA maxIter rules input = or $ go maxIter P0 input []
+runNDPA :: forall t n . (Ord n, Eq t) =>
+	Int -> RuleSet t n -> n -> [t] -> Bool
+runNDPA maxIter rules initialSymbol input = or $ go maxIter P0 input []
 	where
-	go :: Int -> NDPAState -> String -> [Output] -> [Bool]
-	go 0 p i o = error $ "Max iterations reached: " ++ show p ++ ", " ++ input ++ ", " ++ i ++ ", " ++ show o
+	go :: Int -> NDPAState -> [t] -> [Output t n] -> [Bool]
+	go 0 p i o = error "Max iterations reached"
 	go n p i o = do
-			(p', (ni, no)) <- yield rules p i o
+			(p', (ni, no)) <- yield rules initialSymbol p i o
 			-- Stop when it reaches P1 with empty input and stack
 			if (P1, [], []) == (p', ni, no) 
 				then pure True
@@ -99,27 +115,28 @@ runSolution filePath = do
 		Left err -> putStrLn $ "Error :" ++ err
 		Right (rules, samples) -> do
 			--putStrLn $ unlines $ fmap (show . (runNDPA 1000 rules &&& id)) samples
-			print $ DL.foldl' (+) 0 $ fmap (fromEnum . runNDPA 1000 rules) samples
+			print $ DL.foldl' (+) 0 $ fmap (fromEnum . runNDPA 1000 rules 0) samples
 
 			-- Part 2
 			-- Rules 8 and 11 are replaced with
 			-- 8: 42 | 42 8
 			-- 11: 42 31 | 42 11 31
 
-			let alternateRules = rules V.//
-				[ (8, Union (NonTerminal 42) (Concat (NonTerminal 42) (NonTerminal 8)))
-				, (11, Union (Concat (NonTerminal 42) (NonTerminal 31))
-					(Concat (NonTerminal 42) (Concat (NonTerminal 11) (NonTerminal 31))))]
+			let alternateRules = DL.foldl' (\ruleSet (nonTerminal, newRule) ->
+				Map.adjust (const newRule) nonTerminal ruleSet) rules
+					[ (8, Union (NonTerminal 42) (Concat (NonTerminal 42) (NonTerminal 8)))
+					, (11, Union (Concat (NonTerminal 42) (NonTerminal 31))
+						(Concat (NonTerminal 42) (Concat (NonTerminal 11) (NonTerminal 31))))]
 
 			--putStrLn $ unlines $ fmap (show . (runNDPA 1000 alternateRules &&& id)) $ samples
-			print $ DL.foldl' (+) 0 $ fmap (fromEnum . runNDPA 1000 alternateRules) samples
+			print $ DL.foldl' (+) 0 $ fmap (fromEnum . runNDPA 1000 alternateRules 0) samples
 
 
 -- Concatenation takes precedence over union
-parseRules :: Parser (Vector Rule)
-parseRules = V.fromList . fmap snd . DL.sortOn fst <$> DAB.sepBy1' parseRule endOfLine
+parseRules :: Parser (RuleSet Char Int)
+parseRules = Map.fromList . DL.sortOn fst <$> DAB.sepBy1' parseRule endOfLine
 	where
-	parseRule :: Parser (Integer, Rule)
+	parseRule :: Parser (Int, Rule Char Int)
 	parseRule = (,) <$> (decimal <* string ": ") <*> parseUnion
 	parseUnion = DAB.sepBy1' parseConcat (string " | ") >>= \case
 		[r] -> pure r
@@ -129,7 +146,7 @@ parseRules = V.fromList . fmap snd . DL.sortOn fst <$> DAB.sepBy1' parseRule end
 		(r:rs) -> pure $ DL.foldl Concat r rs
 	parseLiteral = Terminal <$> (char '"' *> notChar '"' <* char '"')
 
-parseInput :: Parser (Vector Rule, [String])
+parseInput :: Parser (RuleSet Char Int, [String])
 parseInput = 
 	(,) <$>
 	(parseRules <* endOfLine <* endOfLine) <*>
