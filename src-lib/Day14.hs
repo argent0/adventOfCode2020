a06640e1c96cb5ff37411a74d53742c235c1e4aa
Aner Lucero <4rgento@gmail.com>
2020-12-27 09:35:33 -0300

    Day 14, part 2 and clean up

--- src-lib/Day14.hs
+++ src-lib/Day14.hs
@@ -7,94 +7,112 @@ module Day14 where
 
 import Control.Lens
 import Control.Lens.TH
-
 import qualified Data.ByteString as BS
 import Data.Attoparsec.ByteString (Parser)
 import qualified Data.Attoparsec.ByteString as DAB
-import qualified Data.Attoparsec.ByteString.Char8 as DABC8
-import Data.Attoparsec.ByteString.Char8 (decimal, inClass, digit, endOfLine, char, anyChar, letter_ascii, notChar)
-
+import Data.Attoparsec.ByteString.Char8 (decimal, endOfLine, char)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-
 import qualified Data.List as DL
-import qualified Data.List.Split as DLS
-
-import Debug.Trace
-
-import Control.Arrow
-
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as IA
-
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Either (isRight)
-import Data.Maybe (mapMaybe, catMaybes)
-import Data.Bool (bool)
-
-import Data.Vector (Vector, (!))
+import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-
-import Data.Function (on)
-
-import Control.Monad.State.Strict (State)
-import qualified Control.Monad.State.Strict as State
-
 import qualified Control.Monad as CM
-
 import qualified Control.Foldl as L
-
-import Data.Bifunctor (bimap)
-
-import Data.Complex
-import Data.Functor
-
-import Data.Word
 import Data.Bits
 
+
+-- | A string of 1,0 or X
+--
+-- mask = 111101X010011110100100110100101X0X0X
 type Mask = Vector Char
+
+-- | An instruction writes to memory
+--
+-- mem[2629] = 12036738
 newtype Instruction = Instruction (Int, Int) deriving Show --address value
 
+-- | Each input line either sets the mask or writes to memory
 type Input = Either Mask Instruction
 
+-- | The state of the virtual machine
 data MState = MState
 	{ _memory :: Map Int Int
 	, _mask :: Mask } deriving Show
 makeLenses ''MState
 
+-- | Solve part 1
+--
+-- It only makes sence if the first instruction sets the mask value.
+solver :: [Input] -> Maybe Int
 solver input =  case input of
-		(Left initiaLMask:rest) -> L.fold L.sum $ Map.elems $ (^. memory) $
+		(Left initiaLMask:rest) -> Just $
+			-- Sum all the values in memory
+			L.fold L.sum $ Map.elems $ (^. memory) $
+			-- Interpret all instructions.
 			DL.foldl' folder (MState Map.empty initiaLMask) rest
+		_ -> Nothing
 	where
 	folder :: MState -> Either Mask Instruction -> MState
 	folder acc (Left newMask) = (mask .~ newMask) acc
 	folder acc (Right (Instruction (addr, val))) = (memory %~ modder (applyMask (acc ^. mask) val)) acc
 		where
+		-- Writes the resulting value
 		modder :: Int -> Map Int Int -> Map Int Int
 		modder val = Map.insert addr val
 
+-- | Solve part 2
+--
+-- See coments in part 1
+solver2 :: [Input] -> Maybe Int
+solver2 input =  case input of
+		(Left initiaLMask:rest) -> Just $
+			L.fold L.sum $ Map.elems $ (^. memory) $
+			DL.foldl' folder (MState Map.empty initiaLMask) rest
+		_ -> Nothing
+	where
+	folder :: MState -> Either Mask Instruction -> MState
+	folder acc (Left newMask) = (mask .~ newMask) acc
+	folder acc (Right (Instruction (addr, val))) = (memory %~ modder (acc ^. mask)) acc
+		where
+		-- Writes in all resulting addresses
+		modder :: Mask -> Map Int Int -> Map Int Int
+		modder mask mem = DL.foldl' (\m a -> Map.insert a val m) mem $ applyMask2 mask addr
+
+-- | Apply the mask in part 1
+--
+-- 0 in mask clears the bit
+-- 1 in mask sets the bit
+-- X in mask leaves the bit unchanged
 applyMask :: Mask -> Int -> Int
-applyMask mask org = L.fold (L.prefilter ((/='X') . snd) folder) $
+applyMask mask org =
+	DL.foldl' folder org $
+	filter ((/= 'X') . snd) $
+	-- index each char with its position
 	zip [0..] $ reverse $ Vec.toList mask
 	where
-	folder :: L.Fold (Int, Char) Int
-	folder = L.Fold realFolder org id
-	realFolder :: Int -> (Int, Char) -> Int
-	realFolder v (pos, '1') = setBit v pos
-	realFolder v (pos, '0') = clearBit v pos
+	folder :: Int -> (Int, Char) -> Int
+	folder v (pos, '1') = setBit v pos
+	folder v (pos, '0') = clearBit v pos
 
---applyMask' :: Mask -> Int -> [Int]
-applyMask' mask org = DLS.splitOn "X" $ Vec.toList mask
-	--DL.foldr folder $
---	zip [0..] $ reverse $ Vec.toList mask
---	where
---	folder :: L.Fold (Int, Char) [Int]
---	folder = L.Fold realFolder org id
---	realFolder :: [Int] -> (Int, Char) -> [Int]
---	realFolder v (pos, '1') = setBit v pos
---	realFolder v (pos, '0') = clearBit v pos
-
+-- | Apply the mask in part 2
+--
+-- 0 in mask leaves the bit unchanged
+-- 1 in mask sets the bit
+-- X branches into two posibilities where the bit at that position is 0 or 1.
+applyMask2 :: Mask -> Int -> [Int]
+applyMask2 mask org = expandMask $
+	-- index each char with its position
+	zip [0..] $ reverse $ Vec.toList mask
+	where
+	-- Use the list monad
+	expandMask :: [(Int, Char)] -> [Int]
+	expandMask = CM.foldM folder org
+	folder :: Int -> (Int, Char) -> [Int]
+	-- There are two options when the char is 'X'
+	folder acc (pos, 'X') = [clearBit acc pos, setBit acc pos]
+	-- There is only one option other wise
+	folder acc (pos, '1') = [setBit acc pos]
+	folder acc (pos, '0') = [acc]
 
 parseMask :: Parser Mask
 parseMask =
@@ -111,17 +129,13 @@ parseInput :: Parser [Input]
 parseInput =
 	DAB.many1' (DAB.eitherP parseMask parseInst)
 
-parseQ :: [String] -> Vector Int
-parseQ = Vec.fromList . fmap read
-
 runSolution :: FilePath -> IO ()
 runSolution filePath = do
+	putStrLn "**Day 14**"
 	contents <- BS.readFile filePath
 	let parseResult = DAB.parseOnly parseInput contents
 	case parseResult of
 		Left err -> putStrLn $ "Error :" ++ err
 		Right input -> do
-			print $ applyMask (Vec.fromList "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") 11
-			print $ applyMask (Vec.fromList "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") 101
-			print $ applyMask (Vec.fromList "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") 0
 			print $ solver input
+			print $ solver2 input
